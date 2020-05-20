4eb4621496a395496c9f37d2f988985ce29f7aa6
Tommaso Narracci <t.narracci2@studenti.uniba.it>
2020-05-20 16:48:15 +0200

    aggiornamento classi varie

--- src/main/java/com/mycompany/progetto/Game.java
+++ src/main/java/com/mycompany/progetto/Game.java
@@ -5,31 +5,29 @@
  */
 package com.mycompany.progetto;
 
-import static com.mycompany.progetto.Room.setThisGame;
+import static com.mycompany.progetto.ProjectGameDescription.getRooms;
 
-/**
- *
+/*
  * @author Utente
  */
 public class Game {
 
-    private static Room rooms[] = new Room[10];
-    private static Game thisGame = new Game();
+    private static Room room0,room1;
     public static void main(String[] args) {
         // TODO code application logic here
-        setThisGame(thisGame);
-        rooms[0] = new Room(0, new Room0(), "C:\\Users\\Utente\\Downloads\\imgproject\\CAVE22.jpg", "Caverna", new Room0().backscreen);
-        rooms[1] = new Room(1, new Room1(), "C:\\Users\\Utente\\Downloads\\imgproject\\cave_.jpg", "Boh", new Room1().background);
+        //setThisGame(thisGame);
+        room0 = new Room(0, new Room0(), "C:\\Users\\Utente\\Downloads\\imgproject\\CAVE22.jpg", "Caverna", new Room0().backscreen);
+        room1 = new Room(1, new Room1(), "C:\\Users\\Utente\\Downloads\\imgproject\\cave_.jpg", "Boh", new Room1().background);
+        getRooms().add(room0);
+        getRooms().add(room1);
         //Room room2 = new Room(2, new Room2(), "C:\\Users\\Utente\\Downloads\\imgproject\\CAVE22.jpg", "Caverna", new Room2().background);
-        rooms[0].setNorth(rooms[1]);
-        rooms[0].getThisRoom().setVisible(true);
+        room0.setNorth(room1);
+        room0.getThisRoom().setVisible(true);
         
     }
 
 
-    public Room getmyRoom(int pos){
-        return rooms[pos];
-    }
+
 }
 
 
